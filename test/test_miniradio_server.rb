30b966b17cad8f18cd2136e3221958dc5bd16aae
Koichiro Ohba <koichiro.ohba@gmail.com>
2025-06-04 22:31:53 +0900

    Fix test

--- test/test_miniradio_server.rb
+++ test/test_miniradio_server.rb
@@ -71,6 +71,6 @@ class TestMiniradioServer < Minitest::Test
       MiniradioServer::HLS_SEGMENT_DURATION,
       @dummy_logger
     )
-    assert_equal app.index, "<!DOCTYPE html><html><body><ul><li><a href=\"/stream/eine/playlist.m3u8\">eine.mp3</a> </li></ul></body></html>"
+    assert_equal app.index, "<!DOCTYPE html><html><head><meta charset=\"UTF-8\" /><meta content=\"width=device-width, initial-scale=1.0\" name=\"viewport\" /><title>Miniradio Server</title><link href=\"https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css\" rel=\"stylesheet\" type=\"text/css\" /><link href=\"style/main.css\" rel=\"stylesheet\" type=\"text/css\" /></head><body><h1>Straming List</h1><table class=\"compact striped\"><thead><tr><th scope=\"col\">#</th><th scope=\"col\">Title</th><th scope=\"col\">Artist</th><th scope=\"col\">Album</th></tr></thead><tbody><tr><th scope=\"row\"><a href=\"./#song-1\" id=\"song-1\">1</a> </th><td><a href=\"/stream/eine/playlist.m3u8\">eine</a> </td><td></td><td></td></tr></tbody></table><hr /><p>Miniradio ver 0.0.2</p></body></html>"
   end
 end
